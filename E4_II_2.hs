ff7a390f8b3d95f5e57bf806daa40f85c1d32452
Abraham Medina Carrillo <abraham.medina.carrillo@uabc.edu.mx>
2021-03-12 23:45:45 -0800

    E4_II_2 - Numeros Amigos Finalizado

---
+++ E4_II_2.hs
@@ -0,0 +1,23 @@
+{-
+    Creador: Abraham Medina Carrillo
+    Repositorio: https://github.com/Medina1402/LPD-Haskell 
+    Fecha: 12-03-21
+    
+    Descripcion: *** Números amigos ***
+        Dos números amigos son dos números enteros positivos a y b tales que la suma de los 
+        divisores propios de uno es igual al otro número y viceversa.
+        >>> Ejemplo:
+            220 -> 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 y 110
+            >>> Suman 284
+            284 -> 1, 2, 4, 71 y 142
+            >>> Suman 220
+-}
+
+module E4_II_2 where
+
+numFriends :: Integral a => a -> a -> Bool
+numFriends a b
+    | a == 1 && b == 1 = False
+    | otherwise = do
+        let sumatoria n = sum (init [ x | x <- [1..n], (mod n x) == 0 ])
+        (sumatoria a == b) && (sumatoria b == a)
