53b1504ecb7378eff6bf17cd6c70a186f1768b50
Abraham Medina Carrillo <abraham.medina.carrillo@uabc.edu.mx>
2021-03-14 11:23:42 -0700

    E4_II_4 - Finalizado - arcsin aproximacion Maclaurin

--- E4_II_4.hs
+++ E4_II_4.hs
@@ -4,27 +4,38 @@
     Fecha: 12-03-21
     
     Descripcion: *** Aprocimacion de arcsin ***
-        Mediante la seria de Maclaurin realizaremos una aproximacion de arcsin(0.57)
+        Mediante la seria de Maclaurin realizaremos una aproximacion de arcsin
+        >>> Ejemplo:
+            arcsin(-0.57)
+            >> -0.6065058552130866
+            arcsin 0.57
+            >> 0.6065058552130866
+            arcsin 2
+            >> Valor fuera de rango: -1 >= x <= 1
+            arcsin (-1.000001)
+            >> Valor fuera de rango: -1 >= x <= 1
 -}
 
 module E4_II_4 where
-    
-arcsin :: (Floating p, Ord p) => p -> p
-arcsin 0 = 0
-arcsin x = do
-    let max_iteration = 30
 
-    let factorial' n = do
-        if n == 0 then 1
-        else n * factorial'(n-1)
-    
-    let ntermino n = (x**((2*n)+1)) * ((factorial' (2*n)) / ((4**n) * ((factorial' n)**2) * ((2*n)+1)))
-    --
-    -- opcion utilizando listas
-    --sum (map (\n -> ntermino n) [0..max_iteration])
-    --
-    -- opcion utilizando recursividad
-    let sumatoria' n = do
-        if n < 0 then 0
-        else (ntermino n) + sumatoria'(n-1)
-    sumatoria' max_iteration
+arcsin :: (Floating p, Ord p) => p -> p
+arcsin x
+    | abs(x) > 1  = error "Valor fuera de rango: -1 >= x <= 1"
+    | x == 0      = 0
+    | otherwise   = do
+        let max_iteration = 30
+
+        let factorial' n = do
+            if n == 0 then 1
+            else n * factorial'(n-1)
+        
+        let ntermino n = (x**((2*n)+1)) * ((factorial' (2*n)) / ((4**n) * ((factorial' n)**2) * ((2*n)+1)))
+        --
+        -- opcion utilizando listas
+        --sum (map (\n -> ntermino n) [0..max_iteration])
+        --
+        -- opcion utilizando recursividad
+        let sumatoria' n = do
+            if n < 0 then 0
+            else (ntermino n) + sumatoria'(n-1)
+        sumatoria' max_iteration
